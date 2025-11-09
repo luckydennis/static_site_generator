53f84683ac6a95dca0d7901bc47001de535c22a7
luckydennis <dennishan123@gmail.com>
2025-11-09 12:12:47 -0800

    updating path to ./docs

--- src/main.py
+++ src/main.py
@@ -7,7 +7,6 @@ from copystatic import copy_files_recursive
 from gen_content import generate_page, generate_pages_recursive 
 
 DIR_STATIC = "./static"
-DIR_PUBLIC = "./public"
 template_path= "./template.html"
 dir_path_content = "./content"
 dir_path_public = "./docs"
@@ -19,11 +18,11 @@ def main():
         basepath = sys.argv[1]
 
     print("Deleting public directory...")
-    if os.path.exists(DIR_PUBLIC):
-        shutil.rmtree(DIR_PUBLIC)
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
 
     print("Copying static files to public directory...")
-    copy_files_recursive(DIR_STATIC, DIR_PUBLIC)
+    copy_files_recursive(DIR_STATIC, dir_path_public)
 
     print("Generating page...")
     generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
